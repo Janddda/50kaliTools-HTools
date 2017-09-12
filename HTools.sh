07319e7d9509cb28f5caaa27df471a13fd7c745c
Mehedi Shakeel <mehedihshakeel@gmail.com>
2017-09-13 04:32:17 +0600

    Create HTools.sh

---
+++ HTools.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#Hacking Tools Collection by SSTec Tutorials
+#YouTube Channel : https://www.youtube.com/sstectutorials
+#Website : http://www.sstectutorials.com/
+#Facebook Page : https://web.facebook.com/sstectutorials
+#Author : Mehedi Shakeel
+
+clear
+
+cat  <<  EOF 
+██╗  ██╗████████╗ ██████╗  ██████╗ ██╗     ███████╗
+██║  ██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝
+███████║   ██║   ██║   ██║██║   ██║██║     ███████╗
+██╔══██║   ██║   ██║   ██║██║   ██║██║     ╚════██║
+██║  ██║   ██║   ╚██████╔╝╚██████╔╝███████╗███████║
+╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+                                                   
+
+EOF
+
+mkdir Toolkit
+cd Toolkit
+
+# Git Repos...
+
+git clone https://github.com/secretsquirrel/SigThief.git
+git clone https://github.com/UnkL4b/GitMiner.git
+git clone https://github.com/VirusTotal/qt-virustotal-uploader.git
+git clone https://github.com/DataSploit/datasploit.git
+git clone https://github.com/mcarpenter/afl.git
+git clone https://github.com/Suspicious-Shell-Activity/venom.git
+git clone https://github.com/SpiderLabs/Responder.git
+git clone https://github.com/PowerShellMafia/PowerSploit.git 
+git clone https://github.com/hiteshd/Android-Rootkit.git
+git clone https://github.com/cloudsec/brootkit.git
+git clone https://github.com/mempodippy/vlany.git
+git clone https://github.com/unix-thrust/beurk.git
+git clone https://github.com/nmap/nmap.git
+git clone https://github.com/leviathan-framework/leviathan.git
+git clone https://github.com/n1nj4sec/pupy/tree/master/client
+git clone https://github.com/valyala/goloris.git
+git clone https://github.com/radare/radare2.git
+git clone https://github.com/OffensivePython/Saddam.git
+git clone https://github.com/botherder/virustotal.git
+git clone https://github.com/Microsoft/binskim.git
+git clone https://github.com/0xd4d/dnSpy.git
+git clone https://github.com/lowleveldesign/wtrace.git
+git clone https://github.com/spesmilo/electrum.git
+git clone https://github.com/bitcoin-wallet/bitcoin-wallet.git
+git clone https://github.com/tuxotron/xvwa_lamp_container.git
+git clone https://github.com/JonathanSalwan/Triton.git
+git clone https://github.com/hypsurus/weeman.git
+git clone https://github.com/krmaxwell/tinfoleak
+git clone https://github.com/secretsquirrel/fido.git
+git clone https://github.com/gentilkiwi/mimikatz.git
+git clone https://github.com/SecurityInnovation/AuthMatrix.git
+git clone https://github.com/akheron/jansson
+git clone https://github.com/wolf9466/cpuminer-multi.git
+git clone https://github.com/tsiv/ccminer-cryptonight.git
+git clone https://github.com/evilsocket/bettercap-proxy-modules.git
+git clone https://github.com/Raikia/FiercePhish.git
+git clone https://github.com/adaptivethreat/Empire.git
+git clone https://github.com/gophish/gophish.git
+git clone https://github.com/DhavalKapil/icmptunnel.git
+git clone https://github.com/sqlmapproject/sqlmap.git
+git clone https://github.com/davidpepper/fierce-domain-scanner.git
+git clone https://github.com/Mebus/cupp.git
+git clone https://github.com/digininja/CeWL.git
+git clone https://github.com/iagox86/dnscat2.git
+git clone https://github.com/ryhanson/phishery.git
+git clone https://github.com/JusticeRage/Manalyze.git
+git clone git://git.kali.org/packages/exploitdb.git
+git clone https://github.com/trustedsec/social-engineer-toolkit.git
+git clone https://github.com/roobixx/cowpatty.git
+git clone https://github.com/hashcat/hashcat.git
+git clone https://github.com/laramies/theHarvester.git
+git clone https://github.com/secretsquirrel/the-backdoor-factory.git
+git clone https://github.com/EnableSecurity/wafw00f.git
+git clone https://github.com/stephenfewer/ReflectiveDLLInjection.git
+git clone https://github.com/kstaken/dockerfile-examples.git
+git clone https://github.com/xmendez/wfuzz.git
+git clone https://github.com/pxb1988/dex2jar.git
+git clone git://git.kali.org/packages/dirbuster.git
+git clone https://github.com/deltaxflux/fluxion.git
+git clone git://git.kali.org/packages/joomscan.git
+git clone https://github.com/hexploitable/MEMSCAN.git
+git clone https://github.com/hiddenillusion/AnalyzePDF.git
+git clone https://github.com/smalot/pdfparser.git
+git clone https://github.com/jesparza/peepdf.git
+git clone https://github.com/reverse-shell/routersploit.git
+git clone https://github.com/google/sandbox-attacksurface-analysis-tools.git
+git clone https://github.com/sophron/wifiphisher.git
+git clone https://github.com/wpscanteam/wpscan.git
+git clone https://github.com/Yara-Rules/rules.git
+git clone git://github.com/cuckoosandbox/cuckoo.git
+git clone https://github.com/juansacco/exploitpack.git
+git clone https://github.com/BlackArch/webshells.git
+git clone https://github.com/EgeBalci/HERCULES.git
+git clone https://github.com/EgeBalci/Cminer.git
+git clone https://github.com/EgeBalci/The-Eye.git
+git clone https://github.com/DanMcInerney/wifijammer.git
+
+
+echo FINISH!!! Now ENJOY!!!
+clear
+ls
